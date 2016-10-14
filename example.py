0de7fe85a0c7a8d38323b1b24fbc753f296f057e
Xavi Torelló <info@xaviertorello.cat>
2016-10-14 10:22:58 +0200

    Just start the frontend //not the old API

--- example.py
+++ example.py
@@ -4,9 +4,8 @@ __author__ = 'XaviTorello'
 from orakwlum_frontend import *
 import time
 
-frontend = Frontend(port=8000)
-
-api = Api(host="0.0.0.0", settings="orakwlum_frontend/api/settings.py")
+frontend = Frontend(port=8000, host="127.0.0.1")
+#api = Api(host="0.0.0.0", settings="orakwlum_frontend/api/settings.py")
 
 while True:
     # Do something
